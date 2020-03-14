3c456a32c678d8171466553f6a298d507bb3ad3e
SulaimanAlabbar <sulaimannajeebalabbar@gmail.com>
2020-03-14 12:56:55 +0300

    docker

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,6 @@
 const { Pool } = require("pg");
 const config = require(".");
 
-console.log("config.dbHost", config.dbHost);
-console.log("config.dbPort", config.dbPort);
-console.log("config.dbName", config.dbName);
-console.log("config.dbUser", config.dbUser);
-console.log("config.dbPassword", config.dbPassword);
-
 const dbPool = new Pool({
   host: config.dbHost || "localhost",
   port: config.dbPort || 5432,
