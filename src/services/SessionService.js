c7cb5029a4ef26015e9ed1059aeeef0c40f3863b
sulaiman1073 <sulaimanNajeebAlabbar@gmail.com>
2020-09-25 08:46:09 +0300

    viewers 2

--- src/services/SessionService.js
+++ src/services/SessionService.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const db = require("../config/database");
-const CircDepRes = require("../ranking/cir_dep_resolver");
+const redis = require("../config/redis");
+// const CircDepRes = require("../ranking/cir_dep_resolver");
 
 module.exports.login = async ({ usernameOrEmail, password }) => {
   return db.task(async t => {
@@ -11,12 +12,78 @@ module.exports.login = async ({ usernameOrEmail, password }) => {
 
     if (!user || !(await bcrypt.compare(password, user.password))) return false;
 
-    const loginData = await CircDepRes.getLoginData({ userId: user.id });
+    let loginData = await t.SessionRepository.getLoginData({
+      userId: user.id
+    });
+    const channelIds = Object.keys(loginData.channels);
+    const allViewersIds = new Set();
+
+    for await (const cid of channelIds) {
+      const viewers = await redis.smembers(`viewers:${cid}`);
+      viewers.forEach(allViewersIds.add, allViewersIds);
+
+      loginData = {
+        ...loginData,
+        channels: {
+          ...loginData.channels,
+          [cid]: {
+            ...loginData.channels[cid],
+            viewers
+          }
+        }
+      };
+    }
+
+    const { users } = await t.UserRepository.getUsers({
+      userIds: [...allViewersIds]
+    });
+
+    loginData = {
+      ...loginData,
+      users: {
+        ...loginData.users,
+        ...users
+      }
+    };
 
     return loginData;
   });
 };
 
 module.exports.getLoginData = async ({ userId }) => {
-  return CircDepRes.getLoginData({ userId });
+  return db.task(async t => {
+    let loginData = await t.SessionRepository.getLoginData({ userId });
+    const channelIds = Object.keys(loginData.channels);
+    const allViewersIds = new Set();
+
+    for await (const cid of channelIds) {
+      const viewers = await redis.smembers(`viewers:${cid}`);
+      viewers.forEach(allViewersIds.add, allViewersIds);
+
+      loginData = {
+        ...loginData,
+        channels: {
+          ...loginData.channels,
+          [cid]: {
+            ...loginData.channels[cid],
+            viewers
+          }
+        }
+      };
+    }
+
+    const { users } = await t.UserRepository.getUsers({
+      userIds: [...allViewersIds]
+    });
+
+    loginData = {
+      ...loginData,
+      users: {
+        ...loginData.users,
+        ...users
+      }
+    };
+
+    return loginData;
+  });
 };
