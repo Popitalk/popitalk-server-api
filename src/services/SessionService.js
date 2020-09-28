640e6e8c0d83da0e4225b7acc849a2751e8bd5fa
SKaralius <k.simonas.k@gmail.com>
2020-09-28 16:21:39 +0300

    Trending and discover merged to loginData.

--- src/services/SessionService.js
+++ src/services/SessionService.js
@@ -2,6 +2,8 @@ const bcrypt = require("bcryptjs");
 const db = require("../config/database");
 const redis = require("../config/redis");
 // const CircDepRes = require("../ranking/cir_dep_resolver");
+const { discoverChannels } = require("../services/ChannelService");
+const { trendingChannels } = require("../services/ChannelService");
 
 module.exports.login = async ({ usernameOrEmail, password }) => {
   return db.task(async t => {
@@ -37,6 +39,15 @@ module.exports.login = async ({ usernameOrEmail, password }) => {
     const { users } = await t.UserRepository.getUsers({
       userIds: [...allViewersIds]
     });
+
+    const discoverChannelsData = await discoverChannels({ userId: user.id });
+    const trendingChannelsData = await trendingChannels({ userId: user.id });
+
+    loginData.channels = {
+      channels: loginData.channels,
+      discoverChannels: discoverChannelsData,
+      trendingChannels: trendingChannelsData
+    };
 
     loginData = {
       ...loginData,
@@ -75,6 +86,15 @@ module.exports.getLoginData = async ({ userId }) => {
     const { users } = await t.UserRepository.getUsers({
       userIds: [...allViewersIds]
     });
+
+    const discoverChannelsData = await discoverChannels(userId);
+    const trendingChannelsData = await trendingChannels(userId);
+
+    loginData.channels = {
+      channels: loginData.channels,
+      discoverChannels: discoverChannelsData,
+      trendingChannels: trendingChannelsData
+    };
 
     loginData = {
       ...loginData,
