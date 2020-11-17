a7b6502df5d9d775487ac57f1b9889a7b14ba0db
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-11-17 18:24:02 +0200

    fixed queries to get trending, discover and following channels with the current video playing in each channel

--- src/queries/getTrendingChannels.sql
+++ src/queries/getTrendingChannels.sql
@@ -36,7 +36,9 @@ SELECT
               chans.status = 'Ended'
             THEN
               NULL
-            ELSE (
+            WHEN
+              chans.status = 'Paused'
+            THEN (
               SELECT
                 videos.video_info
               FROM
@@ -47,6 +49,42 @@ SELECT
                 channel_videos.channel_id = chans.id
                 AND channel_videos.queue_position = chans.queue_start_position
                 AND videos.id = channel_videos.video_id
+            )
+            WHEN
+              chans.status = 'Playing'
+            THEN (
+              SELECT
+                vids.video_info
+              FROM (
+                SELECT
+                  x.*,
+                  SUM(x.length) OVER () AS quelen,
+                  SUM(x.cumlen) FILTER (WHERE queue_position = chans.queue_start_position) OVER () AS startlen
+                FROM (
+                  SELECT
+                    videos.length,
+                    videos.video_info,
+                    COALESCE(SUM(videos.length) OVER (ORDER BY channel_videos.queue_position ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING), 0) AS cumlen,
+                    channel_videos.queue_position
+                  FROM
+                    videos
+                  JOIN
+                    channel_videos
+                  ON
+                    channel_videos.video_id = videos.id
+                  WHERE
+                    channel_videos.channel_id = chans.id
+                  ORDER BY
+                    channel_videos.queue_position
+                ) AS x
+                ORDER BY
+                  x.queue_position DESC
+              ) AS vids
+              WHERE
+                vids.cumlen <
+                (((EXTRACT(epoch FROM (NOW() - chans.clock_start_time))::BIGINT) + vids.startlen + (chans.video_start_time)::BIGINT) % vids.quelen)
+              LIMIT
+                1
             )
           END
       )
