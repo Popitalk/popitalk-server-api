4568adb705c7d67caeda45d4e768b4bcec271f39
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2021-01-04 21:05:26 +0200

    fixed get categories query to fetch the right count

--- src/queries/getCategories.sql
+++ src/queries/getCategories.sql
@@ -1,17 +1,11 @@
 SELECT 
-  categories.name, 
-  COUNT(channel_categories.channel_id) 
-    FILTER 
-      (
-        WHERE channel_categories.channel_id IS NOT NULL
-      ) 
-    OVER ()
+  categories.name,
+  COUNT(CASE WHEN channel_categories.category_name IS NOT NULL THEN 0 END) 
 FROM 
-   categories 
+   categories
 LEFT JOIN 
   channel_categories 
 ON 
   categories.name = channel_categories.category_name
 GROUP BY 
-  categories.name, 
-  channel_categories.channel_id
+  categories.name
